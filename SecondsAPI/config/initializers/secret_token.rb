ff4711f0b553a18dd3ab37ef9919db0a1def28c0
ariasja <jason.a.arias@vanderbilt.edu>
2014-06-14 11:53:58 -0700

    initial commit

---
+++ SecondsAPI/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondsAPI::Application.config.secret_key_base = '40c7a6ac7fca5485914b7acd93a45250c0653bdc6c07a015d27cf7d0bbad657e14dbe26754b8a626cee354209055d82a06b1bd5a8efd25a1828d85937840ae73'
---
+++ SecondsAPI/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SecondsAPI::Application.config.session_store :cookie_store, key: '_SecondsAPI_session'
