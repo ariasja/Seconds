98df3db2c8ab94ea774a2380d620b7bb107e07dd
ariasja <jason.a.arias@vanderbilt.edu>
2014-06-14 12:29:57 -0700

    Updated user.rb, post.rb and video.rb

--- SecondsAPI/app/models/post.rb
+++ SecondsAPI/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	validates :caption, length: { maximum: 140 }
+
+	has_one :video
+	belongs_to :user
 end
--- SecondsAPI/app/models/user.rb
+++ SecondsAPI/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true, uniqueness: true
+
+	has_many :posts
+	
 end
