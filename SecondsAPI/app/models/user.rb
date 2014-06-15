6188311c158898788261e117d2576451a4b028c2
ariasja <jason.a.arias@vanderbilt.edu>
2014-06-15 14:37:24 -0700

    Completed Chapter 4 of Mike Hartle Tutorial

--- SecondsAPI/app/models/user.rb
+++ SecondsAPI/app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: true
 
 	has_many :posts
+
+	attr_accessor :first_name, :last_name, :email, :bio
+
+	def initialize(attributes = {})
+    	@first_name  = attributes[:first_name]
+    	@last_name = attributes[:last_name]
+    	@email = attributes[:email]
+    	@bio = attributes[:bio]
+  	end
+
 	
 end
