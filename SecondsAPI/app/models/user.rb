84dc3f424b61d34ee6332aa8179183d0c144410a
ariasja <jason.a.arias@vanderbilt.edu>
2014-06-16 21:55:23 -0700

    Made a Basic User Model. Finished Chapter 6 of Rails Tutorial

--- SecondsAPI/app/models/user.rb
+++ SecondsAPI/app/models/user.rb
@@ -1,18 +1,27 @@
 class User < ActiveRecord::Base
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :email, presence: true, uniqueness: true
+    before_save { self.email = email.downcase }
 
-	has_many :posts
+	validates :first_name, presence: true, length: { maximum: 25 }
+	validates :last_name, presence: true, length: { maximum: 25 }
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
-	attr_accessor :first_name, :last_name, :email, :bio
+    has_secure_password
+    validates :password, length: { minimum: 6 }
 
-	def initialize(attributes = {})
-    	@first_name  = attributes[:first_name]
-    	@last_name = attributes[:last_name]
-    	@email = attributes[:email]
-    	@bio = attributes[:bio]
-  	end
+
+	# has_many :posts
+
+	# attr_accessor :first_name, :last_name, :email, :bio
+
+	# def initialize(attributes = {})
+ #    	@first_name  = attributes[:first_name]
+ #    	@last_name = attributes[:last_name]
+ #    	@email = attributes[:email]
+ #    	@bio = attributes[:bio]
+ #  	end
 
 	
 end
--- SecondsAPI/db/schema.rb
+++ SecondsAPI/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614185904) do
+ActiveRecord::Schema.define(version: 20140617042538) do
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 20140614185904) do
     t.string   "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "videos", force: true do |t|
     t.string   "file"
