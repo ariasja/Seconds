d4c96f4ff75ab8fc6f720212176b1d1d2361c245
ariasja <jason.a.arias@vanderbilt.edu>
2014-06-21 17:10:50 -0700

    Updated User_controller - basic User completed

--- SecondsAPI/app/controllers/users_controller.rb
+++ SecondsAPI/app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
+require 'will_paginate/array' 
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+
 
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page]).sort!
   end
 
   # GET /users/1
@@ -21,13 +27,14 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+        @user = User.find(params[:id])
+
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_create_params)
+    @user = User.new(user_params)
       if @user.save
         sign_in @user
         flash[:success] = "Get Ready to Start Taking Some Sweet Flicks"
@@ -48,17 +55,14 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
 
   private
@@ -68,23 +72,26 @@ class UsersController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def user_create_params
+    def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :bio, :password, :password_confirmation)
     end
-    def user_update_params
-      params.require(:user).permit(:first_name, :last_name, :bio)
-    end
 
-    private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
-    # Before filters
-
+    #Before Filters
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
